7395ac299d62f42616ce2eb97e8643765a1cb2b5
Nayan <dreamerboynayan@gmail.com>
2026-05-24 20:23:17 +0600

    added essential model files and work on userModel file

---
+++ backend/src/models/User.model.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+
+const userSchema = new mongoose.Schema({
+    email: {type: String,required: true,unique: true, lowercase:true,trim: true},
+    password: {type: String, required: true, minlength: 6},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    phone: String,
+    avatar: String,
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+// Hash password before saving 
+
+userSchema.pre('save' , async function (next) {
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next()
+    
+})
+
+export default userSchema;
